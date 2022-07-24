220a7243287e30552f7e16320528a9040764b2f4
italian_goat <81044007+niyangbai@users.noreply.github.com>
2022-07-24 23:00:59 +0200

    Delete group.R

--- temp/group.R
+++
@@ -1,7 +0,0 @@
-rm(list = ls())
-setwd("D:/github/wagegap22")
-library(wagegap22package)
-minwage_state_year <- subset(minimum_wage, year %in% seq(2010, 2020))
-control <- find_dif(2010, 2016)[find_dif(2010, 2016)[["dif"]] == 0, "state"]
-treatment <- find_dif(2010, 2016)[(find_dif(2010, 2013)[["dif"]] == 0) & !(find_dif(2013, 2016)[["dif"]] == 0), "state"]
-
